41b19f816076f5c98e416afb0d0c6cbfb2c7914d
Lucas <laguero90@gmail.com>
2020-10-22 11:04:13 -0300

    commit1

---
+++ varios/Ejercicio_CH_clase1.js
@@ -0,0 +1,11 @@
+
+var num = prompt ("Ingrese un numero");
+
+alert ("su numero ingresado es " + num);
+
+var pepe = {
+  edad: 20,
+  peso : "100 kg",
+}
+
+console.log (pepe);
---
+++ varios/primeraclase.js
@@ -0,0 +1,8 @@
+var username = 'pepe';
+var anio = 2020;
+var lucas = {
+    username: username,
+    anio:anio,
+}
+var nombre = prompt ("ingresa algo al promp")
+alert (username)
